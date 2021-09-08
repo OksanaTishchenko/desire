794e81fa01a57d6e2eaef80e0c3dcf2482b23127
OksanaTishchenko <oksana.tishchenko@ukr.net>
2021-09-08 17:54:24 +0300

    add rightside-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
 
+  $('.header__btn').on('click', function () {
+    console.log('hui')
+    $('.rightside-menu').removeClass('rightside-menu--close')
+  });
+  $('.rightside-menu__close').on('click', function () {
+    $('.rightside-menu').addClass('rightside-menu--close')
+  });
 })
+
